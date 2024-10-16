88b8d305a82aad9d3956f69b12bcd60ee3fc3ed4
mohdbinharun56 <arahaman461@gmail.com>
2024-10-16 15:45:21 +0600

    handle error while data fetch

--- public/app.js
+++ public/app.js
@@ -3,6 +3,10 @@ const current = (cityName = 'Dhaka') => {
     fetch(`http://api.weatherapi.com/v1/current.json?key=d3ae7b0e9c79428eaca92717241510&q=${cityName}&aqi=no`)
         .then(response => response.json())
         .then(data => {
+            if (!data) {
+                throw new Error(`Data not found!`);
+            }
+
             console.log(data);
 
             const country = data.location.country;
@@ -11,6 +15,8 @@ const current = (cityName = 'Dhaka') => {
             const humidity = data.current.humidity;
             const temperature = data.current.temp_c;
             const wind_kph = data.current.wind_kph;
+            const condition = data.current.condition.text;
+            const icon = data.current.condition.icon;
 
             document.getElementById('cityName').innerHTML = cityName;
             document.getElementById('country').innerHTML = country;
@@ -18,8 +24,10 @@ const current = (cityName = 'Dhaka') => {
             document.getElementById('humidity').innerHTML = humidity;
             document.getElementById('windKph').innerHTML = wind_kph;
             document.getElementById('temperature').innerHTML = temperature;
-
+            document.getElementById('condition-weather').innerHTML = condition;
+            document.getElementById('icon').src = icon;
         })
+        .catch(error=>console.log('Error fetching weather data: ',error));
 }
 
 current();
