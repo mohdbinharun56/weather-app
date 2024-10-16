b352ad5a3378f9bf9f6b59f16c062a5f7f9352dd
mohdbinharun56 <arahaman461@gmail.com>
2024-10-16 15:13:13 +0600

    set display all temperature

--- public/app.js
+++ public/app.js
@@ -1,27 +1,30 @@
-// const apiKey = "d3ae7b0e9c79428eaca92717241510";
-// const location = "Dhaka";
-// console.log(location)
 
-// fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`)
-// .then(res=>res.json())
-// .then(data=>console.log(data));
+const current = (cityName = 'Dhaka') => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=d3ae7b0e9c79428eaca92717241510&q=${cityName}&aqi=no`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
 
+            const country = data.location.country;
+            const cityName = data.location.name;
+            const localTime = data.location.localtime;
+            const humidity = data.current.humidity;
+            const temperature = data.current.temp_c;
+            const wind_kph = data.current.wind_kph;
 
-const current = fetch('http://api.weatherapi.com/v1/current.json?key=d3ae7b0e9c79428eaca92717241510&q=Dhaka&aqi=no')
-.then(response=>response.json())
-.then(data=>{
-    console.log(data);
+            document.getElementById('cityName').innerHTML = cityName;
+            document.getElementById('country').innerHTML = country;
+            document.getElementById('localTime').innerHTML = localTime;
+            document.getElementById('humidity').innerHTML = humidity;
+            document.getElementById('windKph').innerHTML = wind_kph;
+            document.getElementById('temperature').innerHTML = temperature;
 
-    const country = data.location.country;
-    const cityName = data.location.name;
-    const localTime = data.location.localtime;
-    const humidity = data.current.humidity;
-    const temperature = data.current.temp_c;
-    const wind_kph = data.current.wind_kph;
+        })
+}
 
-    document.getElementById('cityName').innerHTML = cityName;
-    document.getElementById('country').innerHTML = country;
-    document.getElementById('localTime').innerHTML = localTime;
-    document.getElementById('humidity').innerHTML = humidity;
-    document.getElementById('windKph').innerHTML = wind_kph;
-})
+current();
+
+function weather() {
+    const cityName = document.getElementById('searchLocation').value;
+    current(cityName);
+}
